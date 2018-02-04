3c81a87bee7ef19bf8b94671f4aa40d8987d92f0
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2018-02-04 13:06:32 +0530

    Lyric App using GraphQL

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://salman:salman@ds111258.mlab.com:11258/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
